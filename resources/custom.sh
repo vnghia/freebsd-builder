97825ecc456db957035e25158b13cbb8380869e9
Nghia <git@vnghia.com>
2024-04-18 12:58:34 +0200

    build custom freebsd image (#1)
    * build custom freebsd image
    
    * more size
    
    * fix script
    
    * path

--- resources/custom.sh
+++ resources/custom.sh
@@ -3,3 +3,14 @@
 set -exu
 
 # Add your additional provisioning here for custom VM images.
+
+sudo pkg install -y git curl zip unzip cmake ninja pkgconf bash python311 nasm gmake postgresql16-client llvm16
+
+sudo rm -rf /usr/bin/make
+sudo ln -s /usr/local/bin/gmake /usr/bin/make
+make --version
+
+sudo ln -s /usr/local/bin/python3.11 /usr/local/bin/python3
+python3 --version
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
